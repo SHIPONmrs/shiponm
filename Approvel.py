2529994a984326141df504ff23a4d68e9217085f
SHIPONmrs <107757338+SHIPONmrs@users.noreply.github.com>
2022-07-11 14:32:08 +0600

    Add files via upload

---
+++ Approvel.py
@@ -0,0 +1,54 @@
+#OWNER___MRS_SHIPON_°_420
+
+#APROVAL_SEASION
+
+#THIS_TOOL_IS_PAID
+
+#FIRST_YOU_BUY_AND_USE_PROPERLY
+______
+______
+______
+______
+______
+______
+
+BHAEE74XE74XEX4R
+AC88E74XE74XEX4R
+EH9HE74X6J4X27SE
+#Get Approvel What App:+8801773276144
+
+
+def Subscraption():
+	key1=open('/data/data/com.termux/files/usr/bin/.xce-cov', 'r').read()
+	clear()
+	print(logo)
+	r1=requests.get("https://www.facebook.com/md12qw").text
+	if key1 in r1:
+		os.system('clear')
+		print(logo)
+		Main()
+	else:
+		os.system("clear")
+		print(logo)
+		print("\t \033[1;32m First Get Approvel\033[1;37m ")
+		time.sleep(1)
+		os.system("clear")
+		print(logo)
+		print ("")
+		print(" \033[1;32m ðŸ†‚ðŸ…·ðŸ…¸ðŸ…¿ðŸ…¾ðŸ…½ Toll Paid You Need Get Approved First\033[1;37m\n")
+		print(" \033[1;32m Note : Paid Tolls Free  HA JANI LOG \033[1;37m")
+		print ("")
+		print(" Your Key is Not Approved ")
+		print("")
+		print(" Copy And Send Key To Admin")
+		print ("")
+		print (" Your Key : "+mrs+shipon+key1)
+		print ("")
+		name = input(" Your Name : ")
+		print ("")
+		input(" Press Enter To Send Key")
+		time.sleep(3.5)
+		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
+		os.system('am start https://wa.me/+8801773276144?text=' + tks)
+		Subscraption()        
+Subscraption()
